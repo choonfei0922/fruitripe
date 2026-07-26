46e9f29bafbaff36f0bfbd2b55f1399c1fc16a39
choonfei0922 <liewcf-wp23@student.tarc.edu.my>
2026-07-26 22:13:06 +0800

    supabase

---
+++ lib/core/supabase_config.dart
@@ -0,0 +1,8 @@
+class SupabaseConfig {
+  SupabaseConfig._();
+
+  static const String url = 'https://lgbegzejxgnskptdwpmp.supabase.co';
+
+
+  static const String anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxnYmVnemVqeGduc2twdGR3cG1wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ5NDg5NTYsImV4cCI6MjEwMDUyNDk1Nn0.cEIQCSY4gQaeX9yDnxMblEV8cwitFvHLByGr2vDTPik';
+}
